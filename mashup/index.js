655e3059826ba2918ed019581ff63680313169b1
Joseph <joseph.mcaveety@warwick.ac.uk>
2023-02-06 10:54:04 +0000

    Fix problems

--- mashup/index.js
+++ mashup/index.js
@@ -1,8 +1,11 @@
-const a = 'b';
-console.log('Hello this is mashup js')
+const a = "b";
+console.log("Hello this is mashup js");
+var myStatement = true;
 
-if (myStatement = false)
-
+if (myStatement == false)
 {
-  console.log('This is mashup' + 'strings'); if (true == false) { }
+  console.log("This is mashup strings");
+  if (a == "b") {
+    console.log("hello");
+  }
 }
