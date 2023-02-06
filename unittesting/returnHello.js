9658722d86163a4df874898124ca9290a1608f74
Joseph <joseph.mcaveety@warwick.ac.uk>
2023-02-06 11:04:39 +0000

    Pass unit tests

--- unittesting/returnHello.js
+++ unittesting/returnHello.js
@@ -7,6 +7,15 @@ const generateHelloString = (language = "english") => {
     case "gurmukhi":
       return "ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ";
       break;
+    case "italian":
+      return "Bonjourno"
+      break;
+    case "german":
+      return "Guten Tag"
+      break;
+    case "french":
+      return "Bonjour"
+      break;
     default:
       throw RangeError(`Unknown language ${language}`);
   }
